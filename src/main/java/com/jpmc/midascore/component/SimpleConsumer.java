01f21e0c56dd7cecfcc05efefc1e8a36401e49b3
Lebogang Aphane <leaphanjhb024@student.wethinkcode.co.za>
2025-10-17 18:25:06 +0200

    feat: database logic complete

--- src/main/java/com/jpmc/midascore/component/SimpleConsumer.java
+++ src/main/java/com/jpmc/midascore/component/SimpleConsumer.java
@@ -1,8 +1,7 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.UserRecord;
-import com.jpmc.midascore.foundation.Transaction;
-import com.jpmc.midascore.repos.UserRepo;
+import com.jpmc.midascore.foundation.Transaction;;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -18,12 +17,17 @@ public class SimpleConsumer {
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "group1")
     public void consume(String transaction){
 
+
         System.out.println(transaction);
-        String[] stringTransaction =  transaction.split("=");
-        Transaction transaction1 = new Transaction(Long.valueOf(stringTransaction[2]), Long.valueOf(stringTransaction[4]), Float.valueOf(stringTransaction[6]));
+        String new_trans =  transaction.replaceAll("[^0-9.,]", "");
+        String[] stringTransaction = new_trans.split(",");
+
+        Transaction transaction1 = new Transaction(Long.parseLong(stringTransaction[0]), Long.parseLong(stringTransaction[1]), Float.parseFloat(stringTransaction[2]));
         UserRecord user_sender = userRepo.findById(transaction1.getSenderId());
         UserRecord user_receiver = userRepo.findById(transaction1.getRecipientId());
-
+        System.out.println(user_sender.getBalance());
+        System.out.println(user_receiver.getBalance());
+        System.out.println("after");
         if(user_sender.getBalance() >= transaction1.getAmount()){
 
             user_sender.setBalance(user_sender.getBalance() - transaction1.getAmount());
@@ -32,6 +36,8 @@ public class SimpleConsumer {
             userRepo.save(user_sender);
 
         }
+        System.out.println(user_sender.getBalance());
+        System.out.println(user_receiver.getBalance());
 
     }
 
