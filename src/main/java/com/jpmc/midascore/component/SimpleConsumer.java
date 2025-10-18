59e91f3c8837b8ad0e86613a82a4a67a0ccb31a0
Lebogang Aphane <leaphanjhb024@student.wethinkcode.co.za>
2025-10-18 17:00:40 +0200

    refactor: removed print statements for testing

--- src/main/java/com/jpmc/midascore/component/SimpleConsumer.java
+++ src/main/java/com/jpmc/midascore/component/SimpleConsumer.java
@@ -28,9 +28,7 @@ public class SimpleConsumer {
         Transaction transaction1 = new Transaction(Long.parseLong(stringTransaction[0]), Long.parseLong(stringTransaction[1]), Float.parseFloat(stringTransaction[2]));
         UserRecord user_sender = userRepo.findById(transaction1.getSenderId());
         UserRecord user_receiver = userRepo.findById(transaction1.getRecipientId());
-        System.out.println(user_sender.getBalance());
-        System.out.println(user_receiver.getBalance());
-        System.out.println("after");
+
         if(user_sender.getBalance() >= transaction1.getAmount()){
 
             user_sender.setBalance(user_sender.getBalance() - transaction1.getAmount());
@@ -39,11 +37,6 @@ public class SimpleConsumer {
             userRepo.save(user_sender);
 
         }
-        System.out.println(user_sender.getBalance());
-        System.out.println(user_receiver.getBalance());
-        System.out.println("incentives");
-        System.out.println(userApiClient.postRequest(transaction1));
-
 
     }
 
